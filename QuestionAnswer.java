e09a071cd3eefa77fe817962a98687d346d5900c
Vijay2291 <97454496+Vijay2291@users.noreply.github.com>
2024-06-25 14:32:37 +0530

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,23 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "Who is Our Prime Minister?",
+            "How Many Seats are available in Loksabha?",
+            "What is the Total Percentage of Voting in AndraPradesh Elections 2024?",
+            "How many Questions I asked?"
+    };
+    public static String choices[][]={
+            {"Narendra Modi","Mahendra Modi","Modi Gandhi","Rahul Gandhi"},
+            {"450","550","500","535"},
+            {"79 %","98 %","80 %","56 %"},
+            {"5","4","3","none of the above"}
+    };
+    public static String correctAnswers[]={
+            "Narendra Modi",
+            "550",
+            "80 %",
+            "3"
+    };
+
+}
